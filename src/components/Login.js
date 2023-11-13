238c18623ead0d4303831b2c35820d6d694ead7b
melckzedeck <melckzedeck063@gmail.com>
2023-11-13 14:49:40 +0300

    modified  version  with login and signup forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Modal from 'react-modal';
+import SignUp from './SignUp';
+
+Modal.setAppElement('#root'); // Set the root element for accessibility
 
 export default function Login() {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [signUpModal,setSignupModal] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const openSignupModal = () =>{
+    if(modalIsOpen){
+        setModalIsOpen(false);
+
+        setTimeout(() => {
+            setSignupModal(true);
+        }, 1000);
+    }
+
+    setSignupModal(true)
+  }
+
+  const closeSignupModal =()=>{
+    setSignupModal(false);
+  }
+
   return (
     <div>
-        {/* <!-- component -->
-<link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" /> */}
+
 
 <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
   <div class="flex flex-col bg-white shadow-md px-4 sm:px-6 md:px-8 lg:px-10 py-8 rounded-md w-full max-w-md">
@@ -65,16 +97,28 @@ export default function Login() {
         </div>
       </form>
     </div>
+            
     <div class="flex justify-center items-center mt-6">
-      <a href="#" target="_blank" class="inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs text-center">
+      <button onClick={openSignupModal}  class="inline-flex items-center font-bold text-blue-500 hover:text-blue-700 text-xs text-center">
         <span>
           <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
             <path d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
           </svg>
         </span>
         <span class="ml-2">You don't have an account?</span>
-      </a>
+      </button>
     </div>
+    <Modal
+              isOpen={signUpModal}
+              onRequestClose={closeModal}
+              contentLabel="Example Modal"
+            >
+              <SignUp />
+              <div className="absolute top-1 right-4 bg-red-500">
+
+              <button onClick={closeSignupModal}>Close Modal</button>
+              </div>
+            </Modal>
   </div>
 </div>
     </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,32 +2,45 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>
+    <div className='bg-gray-200'>
         {/* <!-- component --> */}
 <div class="grid min-h-screen place-items-center">
-  <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12">
+  <div class="w-11/12 p-12 bg-white rounded-lg sm:w-8/12 md:w-1/2 lg:w-5/12">
     <h1 class="text-xl font-semibold">Hello there ?, <span class="font-normal">please fill in your information to continue</span></h1>
     <form class="mt-6">
       <div class="flex justify-between gap-3">
         <span class="w-1/2">
           <label for="firstname" class="block text-xs font-semibold text-gray-600 uppercase">Firstname</label>
-          <input id="firstname" type="text" name="firstname" placeholder="John" autocomplete="given-name" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+          <input id="firstname" type="text" name="firstname" placeholder="John" autocomplete="given-name" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
         </span>
         <span class="w-1/2">
           <label for="lastname" class="block text-xs font-semibold text-gray-600 uppercase">Lastname</label>
-        <input id="lastname" type="text" name="lastname" placeholder="Doe" autocomplete="family-name" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
+        <input id="lastname" type="text" name="lastname" placeholder="Doe" autocomplete="family-name" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
         </span>
       </div>
-      <label for="email" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">E-mail</label>
-      <input id="email" type="email" name="email" placeholder="john.doe@company.com" autocomplete="email" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
-      <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
-      <input id="password" type="password" name="password" placeholder="********" autocomplete="new-password" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
-      <label for="password-confirm" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Confirm password</label>
-      <input id="password-confirm" type="password" name="password-confirm" placeholder="********" autocomplete="new-password" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner" required />
-      <button type="submit" class="w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-        Sign up
-      </button>
-      <p class="flex justify-between inline-block mt-4 text-xs text-gray-500 cursor-pointer hover:text-black">Already registered?</p>
+           <label for="email" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">E-mail Address</label>
+           <input id="email" type="email" name="email" placeholder="john.doe@company.com" autocomplete="email" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
+        
+         <label for="telephone" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Telephone</label>
+         <input id="telephone" type="telephone" name="telephone" placeholder="+255710020090" autocomplete="telephone" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
+        
+             <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
+            <input id="password" type="password" name="password" placeholder="********" autocomplete="new-password" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
+         
+            <label for="password-confirm" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Confirm password</label>
+            <input id="password-confirm" type="password" name="password-confirm" placeholder="********" autocomplete="new-password" class="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" required />
+         
+         <div class="flex w-full my-4">
+          <button type="submit" class="flex items-center justify-center focus:outline-none text-white text-sm sm:text-base bg-blue-600 hover:bg-blue-700 rounded py-2 w-full transition duration-150 ease-in">
+            <span class="mr-2 uppercase">Sign Up</span>
+            <span>
+              <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+            </span>
+          </button>
+        </div>
+      <p class="flex justify-between inline-block mt-4 text-xs text-blue-500 cursor-pointer hover:text-black">Already registered?</p>
     </form>
   </div>
 </div>
